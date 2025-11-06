0bcfb65104b80a705a91810dc29a1da39e6c70df
NICOLAS CAMARGO LIZARAZO <98283511+nicocamagic@users.noreply.github.com>
2025-11-06 14:40:26 +0000

    cambios

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-
-const supabaseUrl = 'https://xkaahsgbpruiouyzgzff.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYWFoc2dicHJ1aW91eXpnemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNzQ0MjMsImV4cCI6MjA3NjY1MDQyM30.hVUWNAXjrkyvPsorFGRGLHTPPjSjVlTkg8k24Yt_jo4';
+const supabaseUrl = "https://xkaahsgbpruiouyzgzff.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYWFoc2dicHJ1aW91eXpnemZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNzQ0MjMsImV4cCI6MjA3NjY1MDQyM30.hVUWNAXjrkyvPsorFGRGLHTPPjSjVlTkg8k24Yt_jo4";
 export const supabase = createClient(supabaseUrl, supabaseKey);
